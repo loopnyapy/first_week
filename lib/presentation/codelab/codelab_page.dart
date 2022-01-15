d6afda0de549468b03901b454a820018999cb97e
loopnyapy <da.eto.deniska@gmail.com>
2022-01-15 14:56:03 +0300

    feat: codelab pt1 and pt2

--- lib/presentation/codelab/codelab_page.dart
+++ lib/presentation/codelab/codelab_page.dart
@@ -1,18 +1,81 @@
+import 'package:english_words/english_words.dart';
+import 'package:first_week/presentation/codelab/saved_page.dart';
+import 'package:first_week/presentation/codelab/widgets/word_pair_row.dart';
 import 'package:first_week/presentation/widgets/custom_appbar.dart';
+import 'package:first_week/presentation/widgets/list_view_separated.dart';
 import 'package:first_week/utils/pages.dart';
 import 'package:flutter/material.dart';
 
-class CodelabPage extends StatelessWidget {
+class CodelabPage extends StatefulWidget {
   const CodelabPage({Key? key}) : super(key: key);
 
   @override
+  State<CodelabPage> createState() => _CodelabPageState();
+}
+
+class _CodelabPageState extends State<CodelabPage> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+
+  @override
+  void initState() {
+    super.initState();
+    addWordPairsToList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(
         title: Pages.codelab,
         hasLeading: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => SavedPage(saved: _saved),
+              ),
+            ),
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
       ),
-      body: SizedBox(),
+      body: NotificationListener<ScrollUpdateNotification>(
+        child: CustomListViewSeparated(
+          itemBuilder: (_, index) {
+            var currentSuggestion = _suggestions[index];
+            var alreadySaved = _saved.contains(currentSuggestion);
+
+            return WordPairRow(
+              pair: currentSuggestion,
+              saved: alreadySaved,
+              onTap: () => setState(
+                () {
+                  if (alreadySaved) {
+                    _saved.remove(currentSuggestion);
+                  } else {
+                    _saved.add(currentSuggestion);
+                  }
+                },
+              ),
+            );
+          },
+          itemCount: _suggestions.length,
+        ),
+        onNotification: (notification) {
+          if (notification.metrics.pixels >=
+              notification.metrics.maxScrollExtent) {
+            addWordPairsToList();
+          }
+          return true;
+        },
+      ),
     );
   }
+
+  void addWordPairsToList() => setState(() {
+        _suggestions.addAll(generateWordPairs().take(20));
+      });
 }
---
+++ lib/presentation/codelab/saved_page.dart
@@ -0,0 +1,38 @@
+import 'package:english_words/english_words.dart';
+import 'package:first_week/presentation/widgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+
+class SavedPage extends StatelessWidget {
+  const SavedPage({Key? key, required this.saved}) : super(key: key);
+
+  final Set<WordPair> saved;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(
+        title: 'Saved Suggestions',
+        hasLeading: true,
+      ),
+      body: ListView(
+        children: saved.isNotEmpty
+            ? ListTile.divideTiles(
+                context: context,
+                tiles: saved.map(
+                  (pair) => ListTile(
+                    title: Text(pair.asPascalCase,
+                        style: Theme.of(context).textTheme.subtitle1),
+                  ),
+                ),
+              ).toList()
+            : [
+                const Center(
+                  child: Text(
+                    "There's no saved suggestion yet! Go change it! 😎",
+                  ),
+                )
+              ],
+      ),
+    );
+  }
+}
